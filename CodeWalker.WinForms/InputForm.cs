6f780cbde543bebb5c28e9529f450e97ed55899a
station <station@oali.uk>
2026-03-31 18:25:11 +0800

    InputForm.cs

---
+++ CodeWalker.WinForms/InputForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeWalker.WinForms;
+
+public partial class InputForm : Form
+{
+    public InputForm()
+    {
+        InitializeComponent();
+    }
+}
